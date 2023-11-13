03a4a28d66240d2d4a66c9967c7253346875b357
Megha060199 <megha.g.chandwani@gmail.com>
2023-11-13 10:59:46 -0500

    Initial Commit

---
+++ ebayApp/ebayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ebayAppApp.swift
+//  ebayApp
+//
+//  Created by Megha Chandwani on 13/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ebayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
